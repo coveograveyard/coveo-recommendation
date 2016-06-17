295ceb584b07e031fbdd5611e2251704333f95ef
Mathieu Carpentier <mcarpentier@coveo.com>
2016-06-17 15:13:34 -0400

    Redirection / to /Index.html

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var port = process.env.PORT || 8080;
 app.use('/search', express.static(process.cwd() + '/node_modules/coveo-search-ui/bin/'))
 app.use(express.static(process.cwd() + '/pages'));
 
+app.get('/', function(req, res){
+  res.redirect('/Index.html');
+})
+
 app.listen(port, function(){
   console.log('Listening to port: ' + port);
 });
