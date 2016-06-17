36b02ca390d6e1fed317ad29b76f20ec083a23ef
Mathieu Carpentier <mcarpentier@coveo.com>
2016-06-17 14:38:11 -0400

    Fix server after cleanup

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use('/search', express.static(__dirname + '/../bin'))
-app.use(express.static(__dirname));
+app.use('/search', express.static(__dirname + 'node_modules/coveo-search-ui/bin'))
+app.use(express.static(__dirname + '/pages'));
 
 app.listen(port, function(){
   console.log('Listening to port: ' + port);
