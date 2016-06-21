cddec8f04a8387359b5abfa0cb4e8ec661c98f32
Mathieu Carpentier <mcarpentier@coveo.com>
2016-06-21 15:56:30 -0400

    Use coveo.analytics from npm

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.use('/search', express.static(process.cwd() + '/node_modules/coveo-search-ui/bin/'))
-app.use('/lib', express.static(process.cwd() + '/lib/'))
+app.use('/lib', express.static(process.cwd() + '/node_modules/coveo.analytics/dist/'))
 app.use(express.static(process.cwd() + '/pages'));
 
 app.get('/', function(req, res){
